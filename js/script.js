26d88108adb65dbf35869fc2c233c0347cc7f5ee
Tanvir Mazharul <tanvirmazharul04@gmail.com>
2023-06-21 16:09:23 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    var swiper = new Swiper(".mySwiper", {
+      cssMode: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      mousewheel: true,
+      keyboard: true,
+    });
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel();
+});
+
+  /**Demo 1*/
+    var arc = function (t)
+    {
+        return {
+            x: 500.0 + 300.0 * Math.cos(2.0 * Math.PI * t - 0.5 * Math.PI),
+            y: 400.0 + 300.0 * Math.sin(2.0 * Math.PI * t - 0.5 * Math.PI)
+        };
+    };
+
+    $('#ctext').curvedText({
+        curve:    arc,
+        domain:   [-0.2, 0.2],
+        viewport: { x: 0.0, y: 0.0, width: 1000.0, height: 400.0 }
+    });
+});
